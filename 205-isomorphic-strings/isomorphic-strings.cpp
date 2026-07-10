422f83675602f28085bd273febb835fff6d50a25
Bhumik Sharma <bhumikkannu@gmail.com>
2026-07-10 19:10:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (56.83%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char>v;
-        int n1=s.size();
-        int n2=t.size();
-        if(n1!=n2)return false;
-        for(int i=0;i<n1;i++){
-           if(v.find(s[i])==v.end()){
-            v[s[i]]=t[i];
-           }
-           else if(v[s[i]]!=t[i])return false;
+        // Arrays initialized to 0 to store the "last seen index + 1"
+        int mapS[256] = {0};
+        int mapT[256] = {0};
+        
+        int n = s.size(); // Since lengths are equal (or checked), we use one variable
+        
+        for (int i = 0; i < n; i++) {
+            // If the "last seen" positions don't match, they aren't isomorphic
+            if (mapS[s[i]] != mapT[t[i]]) {
+                return false;
+            }
+            
+            // Mark the current position (using i + 1 to distinguish from the initial 0)
+            mapS[s[i]] = i + 1;
+            mapT[t[i]] = i + 1;
         }
-        unordered_map<char,char>b;
-        for(int i=0;i<n1;i++){
-           if(b.find(t[i])==b.end()){
-            b[t[i]]=s[i];
-           }
-           else if(b[t[i]]!=s[i])return false;
-        }
+        
         return true;
     }
 };
