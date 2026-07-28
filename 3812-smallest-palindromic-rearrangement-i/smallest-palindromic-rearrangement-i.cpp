bf75e976147cf269e5eef697681e0d02bdc5a04f
Bhumik Sharma <bhumikkannu@gmail.com>
2026-07-28 11:48:48 +0530

    Time: 11 ms (98.46%) | Memory: 54 MB (95.38%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -3,9 +3,17 @@ public:
     string smallestPalindrome(string s) {
     int n=s.size();
     int len=n/2;
-    sort(s.begin(),s.begin()+len);
-    for(int i=0;i<len;i++){
-        s[n-i-1]=s[i];
+    vector<int>v(26,0);
+    for(int i=0;i<len;i++)
+    v[s[i]-'a']++;
+    
+    int l=0,r=n-1;
+    for(int i=0;i<26;i++){
+        while(v[i]){
+            s[l++]=i+'a';
+            s[r--]=i+'a';
+            v[i]--;
+        }
     }
     return s;
     }
