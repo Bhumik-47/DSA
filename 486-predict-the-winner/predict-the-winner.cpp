ef5185ba9fc5362804408fddc96ccd08128adda4
Bhumik Sharma <bhumikkannu@gmail.com>
2026-08-01 12:33:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (96.48%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,24 +1,20 @@
 class Solution {
-public:
-    bool rec(vector<int>&nums , int i, int j, int p1, int p2, int ch){
-        if(i<=j){
-            if(ch){
-                return rec(nums,i+1,j,p1+nums[i],p2,0)||rec(nums,i,j-1,p1+nums[j],p2,0);
-            }
-            else{
-                return rec(nums,i+1,j,p1,p2+nums[i],1)&&rec(nums,i,j-1,p1,p2+nums[j],1);
-            }
-            
-        }
-    
-      return p1>=p2;
-    
+    int memo[21][21];
+
+    int getNetScore(vector<int>& nums, int i, int j) {
+        if (i == j) return nums[i];
+        if (memo[i][j] != -1) return memo[i][j];
+
+        // Pick left element OR pick right element
+        int pickLeft  = nums[i] - getNetScore(nums, i + 1, j);
+        int pickRight = nums[j] - getNetScore(nums, i, j - 1);
+
+        return memo[i][j] = max(pickLeft, pickRight);
     }
+
+public:
     bool predictTheWinner(vector<int>& nums) {
-        int n=nums.size();
-        int i=0,j=n-1,ch=1;
-        int p1=0,p2=0;
-        return rec(nums,i,j,p1,p2,ch);
-        
+        memset(memo, -1, sizeof(memo));
+        return getNetScore(nums, 0, nums.size() - 1) >= 0;
     }
 };
