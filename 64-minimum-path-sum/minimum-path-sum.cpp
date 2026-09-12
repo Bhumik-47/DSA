b080e47f85c66de534f7f88e110c368e90dc35a7
Bhumik Sharma <bhumikkannu@gmail.com>
2026-09-12 13:34:53 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (41.69%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-     long long getMinSum(vector<vector<int>>& grid, int i, int j,vector<vector<int>>&memo){
+    int fun(vector<vector<int>>& grid,vector<vector<int>>& dp,int m, int n){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0)dp[i][j]=grid[i][j];
+                else
+                 if(i==0)dp[i][j]=grid[i][j]+dp[i][j-1];
+                else if(j==0)dp[i][j]=grid[i][j]+dp[i-1][j];
+                else {
+                    dp[i][j]=grid[i][j]+min(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m-1][n-1];
+     }
+
+    int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        if(memo[i][j]!=-1)return memo[i][j];
-        if(i==0 && j==0)return grid[0][0];
-        else if(i==0){memo[i][j]=grid[i][j]+getMinSum(grid,0,j-1,memo);
-        return memo[i][j];
-        }
-        else if(j==0){memo[i][j]= grid[i][j]+getMinSum(grid,i-1,0,memo);
-        return memo[i][j];
-        }
-        else{
-            
-            memo[i][j]=grid[i][j]+min(getMinSum(grid,i-1,j,memo),getMinSum(grid,i,j-1,memo));
-            return memo[i][j];
-        }
-    }
-    int minPathSum(vector<vector<int>>& grid) { 
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<int>>memo(m,vector<int>(n,-1));
-        return getMinSum(grid,m-1,n-1,memo);
-        
+        vector<vector<int>>dp(m,vector<int>(n+1,0));
+        return fun(grid,dp,m,n);
     }
 };
