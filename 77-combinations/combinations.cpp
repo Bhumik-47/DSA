1e4d56fe4388032bd0a7929376b91671980c1699
Bhumik Sharma <bhumikkannu@gmail.com>
2026-09-18 20:40:28 +0530

    Time: 63 ms (55.69%) | Memory: 109.8 MB (25.28%) - LeetSync

---
+++ 77-combinations/combinations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void com(int n, int k , vector<int>&v,int i, vector<vector<int>>&ans){
+         if(v.size()==k){
+        ans.push_back(v);
+        return;
+        }
+       if(i>n){
+        return;
+       }
+       
+        v.push_back(i);
+        com(n,k,v,i+1,ans);
+        v.pop_back();
+        com(n,k,v,i+1,ans);
+
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int>v;
+        int i=1;
+        vector<vector<int>>ans;
+        com(n,k,v,i,ans);
+        return ans;
+    }
+};
